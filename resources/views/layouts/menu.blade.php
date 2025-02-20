90566f18eb4c3feeb8b911605966ae4a7092dc00
Alonso <alonso.rbass@gmail.com>
2025-02-20 15:16:30 +0100

    feat: test with urls and quick css fix

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,10 +1,10 @@
-<nav class="container_menu">
-    <header>
+<header class="container_menu">
+    <nav>
         <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">Products</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
         <button><a href="#">Login</a></button>
-    </header>
-</nav>  
+    </nav>
+</header>  
