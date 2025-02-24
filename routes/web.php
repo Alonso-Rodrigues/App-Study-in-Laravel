4c65480e42bf7102bc1a9300177bab7a42c38fcf
Alonso <alonso.rbass@gmail.com>
2025-02-24 01:05:44 +0100

    bug: to render events

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index'] );
 
-Route::get('/events', [EventController::class, 'events'] );
-
-Route::get('/products', [EventController::class, 'products']);
+Route::get('/events/create', [EventController::class, 'create'] );
 
 Route::get('/contact', [EventController::class, 'contact']);
 
