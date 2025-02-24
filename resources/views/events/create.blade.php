cafbd7627948ce1be4a3a3f1c1c989ab42b97646
Alonso <alonso.rbass@gmail.com>
2025-02-24 19:13:13 +0100

    wip: initial work on event creation form

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,11 +1,30 @@
 @extends('home')
 
-@section('title', 'Create Events Page') 
+@section('title', 'Create Events Page')
 
 @section('content')
 
 <section class="container_create">
-    
+  <section class="create_content">
+    <h1 id="create-event-title"> Create Your Event!</h1>
+    <form class="form-create-events" action="/events" method="POST">
+      <label class="create-event-label" for="title">Event:
+      </label>
+      <input class="input-form-create" type="text" id="title" placeholder="Event Name">
+      
+        <label class="create-event-label" for="title">Event: </label>
+        <input class="input-form-create" type="text" id="title" placeholder="Event Name">
+      
+        <label class="create-event-label" for="title">Event: </label>
+        <input class="input-form-create" type="text" id="title" placeholder="Event Name">
+      
+        <label class="create-event-label" for="title">Event: </label>
+        <input class="input-form-create" type="text" id="title" placeholder="Event Name">
+    </form>
+  </section>
 </section>
-    
+  <section class="container_create">
+
+  </section>
+
 @endsection
