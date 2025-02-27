4a3eb0fefd44c8c2712efcfabf170fe536850190
Alonso <alonso.rbass@gmail.com>
2025-02-27 23:59:17 +0100

    feat: build upload images

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -25,6 +25,15 @@ class EventController extends Controller
       $event->private = $request->private;
       $event->description = $request->description;
 
+      //Image Upload
+      if($request->hasFile('image') && $request->file('image')->isValid()){
+
+         $requestImage = $request->image;
+         $extension = $requestImage->extension();
+         $imageName = md5($requestImage->image->getClientOriginalName() . strtotime('now')) . "." . $extension;
+         $request->image->move(public_path('img/events'));
+      }
+
       $event->save();
       return redirect('/')->with('msg','Event created successfully!');
    }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -7,25 +7,29 @@
 <section class="create_container">
   <section class="create_content">
     <h1 id="create-event-title"> Create Your Event!</h1>
-    <form class="create-form-events" action="/events" method="POST">
+    <form class="create-form-events" action="/events" method="POST" enctype="multipart/form-data">
       @csrf {{-- blade directive against Cross-site request forgery attacks --}}
+
+      <label class="create-event-label" for="img">Event image</label>
+      <input class="create-input-img" type="file" id="image" name="image">
+      
       <label class="create-event-label" for="title">Event</label>
       <input class="create-input-form" type="text" id="title" name="title" placeholder="Event Name">
       
-        <label class="create-event-label" for="title">City</label>
-        <input class="create-input-form" type="text" id="city" name="city" placeholder="Local Event">
+      <label class="create-event-label" for="title">City</label>
+      <input class="create-input-form" type="text" id="city" name="city" placeholder="Local Event">
       
-        <label class="create-event-label" for="title">Is this event private?</label>
-        <select class="create-event-select" name="private" id="private">
-          <option>Selection</option>
-          <option value="0">No</option>
-          <option value="1">Yes</option>
-        </select>
+      <label class="create-event-label" for="title">Is this event private?</label>
+      <select class="create-event-select" name="private" id="private">
+        <option>Selection</option>
+        <option value="0">No</option>
+        <option value="1">Yes</option>
+      </select>
         
-        <label class="create-event-label" for="title">Description</label>
-        <textarea class="create-textarea-form" type="text" name="description" id="description" placeholder="What will your event be like?"></textarea>
+      <label class="create-event-label" for="title">Description</label>
+      <textarea class="create-textarea-form" type="text" name="description" id="description" placeholder="What will your event be like?"></textarea>
 
-        <button class="btn-create" type="submit" value="create event">Submit</button>
+      <button class="btn-create" type="submit" value="create event">Submit</button>
     </form>
   </section>
 </section>
