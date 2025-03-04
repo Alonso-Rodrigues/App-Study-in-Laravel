c1f759696eef2a90ef788123c80260becf4c6a85
Alonso <alonso.rbass@gmail.com>
2025-03-05 00:19:48 +0100

    feat: fix CSS show page

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -12,67 +12,36 @@
       </section>
 
       <section class="show-info">
-        <h3 id="show-city">
-          <ion-icon name="pin-outline"></ion-icon>{{ $event->city }}
-        </h3>
-        <p id="show-date">{{ date('d/m/y', strtotime($event->date)) }}</p>
-        <p id="show-participants">
-          <ion-icon name="people-outline"></ion-icon> Participants
-        </p>
-        <h3 id="show-include">The event will include:</h3>
-        <ul class="show-items">
-          @foreach($event->items as $item)
-          <p>
-            <ion-icon name="checkmark-circle-outline"></ion-icon>{{ $item }}
+
+        <section class="show-items">
+          <h3>The event will include</h3>
+          <ul class="show-items-ul">
+            @foreach($event->items as $item)
+            <li class="show-items-li">
+              <ion-icon name="checkmark-circle-outline"></ion-icon>{{ $item }}
+            </li>
+            @endforeach
+          </ul>
+        </section>
+
+        <section class="show-date">
+          <h3 id="show-city">
+            <ion-icon name="pin-outline"></ion-icon>{{ $event->city }}
+          </h3>
+          <p id="show-date">{{ date('d/m/y', strtotime($event->date)) }}</p>
+          <p id="show-participants">
+            <ion-icon name="people-outline"></ion-icon> Participants
           </p>
-          @endforeach
-        </ul>
-        <button class="btn-show"><a href="#">Subscribe</a></button>
+        </section>
+
       </section>
 
       <section class="show-description">
         <p><ion-icon name="star-outline"></ion-icon> Producer</p>
         <p>{{ $event->description }}</p>
       </section>
-      
+      <button class="btn-show"><a href="#">Subscribe</a></button>
+
     </section>
-  </section> 
+  </section>
 @endsection
-
-
-
-
-
-
-
-
-<!-- 
-  <section class="show-container">
-    <section class="show-content">
-      <img class="show-img" src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
-      <section class="show-info">
-        <h1 id="show-title">{{ $event->title }}</h1>
-        <h3 id="show-city">
-          <ion-icon name="pin-outline"></ion-icon>{{ $event->city }}
-        </h3>
-        <p id="show-date">{{ date('d/m/y', strtotime($event->date)) }}</p>
-        <p id="show-participants">
-          <ion-icon name="people-outline"></ion-icon> Participants
-        </p>
-        <h3 id="show-city">The event will include:</h3>
-        <ul class="show-items">
-          @foreach($event->items as $item)
-          <p>
-            <ion-icon name="checkmark-circle-outline"></ion-icon>{{ $item }}
-          </p>
-          @endforeach
-        </ul>
-        <button class="btn-show"><a href="#">Subscribe</a></button>
-      </section>
-      <section class="show-description">
-        <p><ion-icon name="star-outline"></ion-icon> Producer</p>
-        <p>{{ $event->description }}</p>
-      </section>
-    </section>
-  </section> 
--->
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,10 +1,10 @@
-<header class="container_menu">
-    <nav>
-        <ul class="menu">
-            <li><a href="/">Home</a></li>
-             <li><a href="/events/create">Create Events</a></li>
-             <li><a href="/events/create">Register</a></li>
-            <li><a href="/contact">Contact</a></li>
+<header class="container-menu">
+    <nav class="menu-nav">
+        <ul class="menu-ul">
+            <li class="menu-li"><a href="/">Home</a></li>
+             <li class="menu-li"><a href="/events/create">Create Events</a></li>
+             <li class="menu-li"><a href="/events/create">Register</a></li>
+            <li class="menu-li"><a href="/contact">Contact</a></li>
         </ul>
         <button class="btn-geral"><a href="#">Login</a></button>
     </nav>
