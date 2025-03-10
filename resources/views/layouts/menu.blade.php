2b8c24de360ef261266217860f26216fb21d322a
Alonso <alonso.rbass@gmail.com>
2025-03-10 12:15:37 +0100

    feat: create login system

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,10 +2,30 @@
     <nav class="menu-nav">
         <ul class="menu-ul">
             <li class="menu-li"><a href="/">Home</a></li>
-             <li class="menu-li"><a href="/events/create">Create Events</a></li>
-             <li class="menu-li"><a href="/events/create">Register</a></li>
+            <li class="menu-li"><a href="/events/create">Create Events</a></li>
+            @auth
+                <li class="menu-li"><a href="/events">My Events</a></li>
+            @endauth
+
+            @guest
+                <li class="menu-li"><a href="/register">Register</a></li>
+            @endguest
+            
             <li class="menu-li"><a href="/contact">Contact</a></li>
         </ul>
-        <button class="btn-geral"><a href="#">Login</a></button>
+        @guest
+            <button class="btn-geral"><a href="/login">Login</a></button>
+        @endguest
+        @auth
+        <button class="btn-geral">
+            <form action="/logout" method="POST">
+                @csrf
+                <a href="/logout" onClick="event.preventDefault();
+                this.closest('form').submit();">
+                    Log Out
+                </a>
+            </form>
+        </button>    
+        @endauth   
     </nav>
 </header>  
