77fc64a44bb81d8b8be53c062ed58ec6e858d5e2
Alonso <alonso.rbass@gmail.com>
2025-03-11 20:51:56 +0100

    feat: create contact page CSS

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -3,8 +3,14 @@
 @section('title', 'Contact Page')
 
 @section('content')
-    <section class="container_contact">
-      <h1>Contact us</h1>
-      <a href="/">Back to Home</a>
+    <section class="container-contact">
+      <section class="container-contact-section">
+        <h1 id="container-contact-title">Contact us</h1>
+        <p class="container-contact-content">
+          The project was developed in Laravel to create an event system where users can search, view and manage events. Components were implemented to display events, perform dynamic searches, and structure the interface with Blade templates.
+          Recently, Jetstream was added for authentication and login management, bringing greater security and convenience for users to access their accounts.
+        </p>
+        <a id="container-contact-home" href="/">Back to home</a>
+      </section>
     </section>
 @endsection
