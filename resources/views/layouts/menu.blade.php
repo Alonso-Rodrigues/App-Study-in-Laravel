e6f6e73b8fe70996a3697725f99045dec0802414
Alonso <alonso.rbass@gmail.com>
2025-03-20 21:43:42 +0100

    feat: CSS menu adjust

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,8 @@
 <header class="container-menu">
     <nav class="menu-nav">
+        @auth
+            <p id="user-name">Welcome: {{ $user->name }}</p>
+        @endauth
         <ul class="menu-ul">
             @auth
                 <li class="menu-li"><a href="/dashboard">Dashboard</a></li>
@@ -10,8 +13,8 @@
                 <li class="menu-li"><a href="/">Home</a></li>
             @endguest
             
-                <li class="menu-li"><a href="/events/create">Create Events</a></li> 
-                
+                <li class="menu-li"><a href="/events/create">Create Events</a></li>
+
             @guest
                 <li class="menu-li"><a href="/register">Register</a></li>
                 <li class="menu-li"><a href="/contact">Contact</a></li>
@@ -19,11 +22,11 @@
         </ul>
 
         @guest
-            <button class="btn-geral"><a href="/login">Login</a></button>
+            <button class="btn-menu"><a href="/login">Login</a></button>
         @endguest
 
         @auth
-            <button class="btn-geral">
+            <button class="btn-menu">
                 <form action="/logout" method="POST">
                     @csrf
                     <a href="/logout" 
